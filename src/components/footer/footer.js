b98605d1f48a72565353780b22655f5fd8d83c3f
pradeepgajanoor <pradeepgajanoor@gmail.com>
2024-02-13 18:42:59 +0530

    UI enhancements

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -5,7 +5,7 @@ function Footer(){
     return <>
     <div className='footer-container'>
        <p>Copyright <a href='https://www.upgrad.com/' target='_blank'>@Upgrad</a>  2024</p>
-       <p>PLEASE NOTE: instructions were not given for the backend code integration nor in the live sessions, so we have used the APIs from <a href='https://fakestoreapi.com/' target='_blank'>https://fakestoreapi.com/</a></p>
+       <p>PLEASE NOTE: We have used the APIs from <a href='https://fakestoreapi.com/' target='_blank'>https://fakestoreapi.com/</a></p>
 
     </div>
     </>
